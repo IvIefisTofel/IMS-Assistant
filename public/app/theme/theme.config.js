3bc9e91b03c8093693f0b4bf582962b1abf2add9
MefisTofel <iviefistofel@gmail.com>
2017-02-07 08:53:21 +0400

    Исправил конфликты bower, добавил режим обновления сайта

--- public/app/theme/theme.config.js
+++ public/app/theme/theme.config.js
@@ -9,7 +9,10 @@
     .config(config);
 
   /** @ngInject */
-  function config(baConfigProvider, colorHelper, $httpProvider, uiSelectConfig, IdleProvider) {
+  function config(baConfigProvider, $locationProvider, $httpProvider, uiSelectConfig, IdleProvider) {
+    $locationProvider.html5Mode(false);
+    $locationProvider.hashPrefix('');
+
     $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
     $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
     uiSelectConfig.theme = 'bootstrap';
